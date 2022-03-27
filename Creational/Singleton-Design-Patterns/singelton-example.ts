478e66a4a198078e4355ea41909b1de54a2f437d
saif hany <saifhany898@gmail.com>
2022-03-27 19:55:02 +0200

    add code "creational/Singleton-Design-Patterns/Logger.js" adding  Logger class ts

---
+++ Creational/Singleton-Design-Patterns/singelton-example.ts
@@ -0,0 +1,22 @@
+class MySingleton {
+
+    static instance: MySingleton;
+
+    private constructor() {
+        console.log("constructor called!");
+    }
+
+    public static getInstance(): MySingleton {
+        if (!MySingleton.instance) {
+            MySingleton.instance = new MySingleton();
+        }
+        return MySingleton.instance;
+    }
+
+    public logic() {
+        console.log("my logic!");
+    }
+}
+
+let myInstance: MySingleton = MySingleton.getInstance();
+myInstance.logic();
